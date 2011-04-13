900af5808518c244dcae6fe66941e83ad0f1f6dd
Garrison Locke <gplocke@gmail.com>
2011-04-13 11:46:36 -0400

    Added a simple service wrapper for the NC State Dining API that is provided by OIT

---
+++ Ncstate/Service/Dining/Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Set of classes to programatically communicate with the NC State University
+ * Dining API
+ *
+ * @package Ncstate_Dining
+ * @see     http://webapps.ncsu.edu/dining
+ * @author  Office of Information Technology - Outreach Technology
+ */
+
+/**
+ * Exception class thrown by any of the services in this library
+ */
+class Ncstate_Service_Dining_Exception extends Exception
+{}
